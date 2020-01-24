0b040188ace77bf549e286178e8e5b116cdbe12c
guiferviz <guiferviz@gmail.com>
2020-01-24 16:16:12 +0100

    Working directory can contain the output dir so it should be writable.

--- docker/run.sh
+++ docker/run.sh
@@ -1,10 +1,10 @@
 
 docker run \
-    -v $PWD:/workspace:ro \
     -v /usr/share/anki:/usr/share/anki:ro \
     -v ~/.ssh:/root/.ssh:ro \
     -v ~/.gitconfig:/root/.gitconfig:ro \
     -v ~/.gh_key:/root/.gh_key:ro \
+    -v $PWD:/workspace \
     -v ~/.local/share/Anki2:/root/.local/share/Anki2 \
     -v ~/.mnemocards:/root/.mnemocards \
     -v ~/learning:$HOME/learning \
