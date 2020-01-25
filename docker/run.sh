693e1a6bc36f7e669ca6c2affa34c763f85178e1
guiferviz <guiferviz@gmail.com>
2020-01-25 01:10:13 +0100

    Preparing docker script for running using root user and change user later.

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,16 @@
 
-docker run \
+CARDS_DIR=$HOME/learning
+
+# Ensures that the config file exists.
+# If it does not exists docker is going to mount a directory instead of a file
+# and the program is going to fail.
+# Also, creating the file here it's going to be owned by the current user.
+if [ ! -f ~/.mnemocards ]; then
+    # Create a file with a valid JSON.
+    echo {} > ~/.mnemocards
+fi
+
+sudo docker run \
     -v /usr/share/anki:/usr/share/anki:ro \
     -v ~/.ssh:/root/.ssh:ro \
     -v ~/.gitconfig:/root/.gitconfig:ro \
@@ -7,9 +18,12 @@ docker run \
     -v $PWD:/workspace \
     -v ~/.local/share/Anki2:/root/.local/share/Anki2 \
     -v ~/.mnemocards:/root/.mnemocards \
-    -v ~/learning:$HOME/learning \
-    --user $(id -u):$(id -g) \
+    -v $CARDS_DIR:$CARDS_DIR \
     -it \
     guiferviz/mnemocards \
     "$@"
+
+# Change ownership of the generated files (assuming that the defaul group of
+# the user is the same as the username)
+sudo chown -R $USER:$USER $CARDS_DIR
 
