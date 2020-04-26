4b5f1c960d708514c3adfc723ffe51a0589ef48c
guiferviz <guiferviz@gmail.com>
2020-04-26 18:45:08 +0200

    Install without installing requirements first, cloning GIT repo.

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -7,3 +7,11 @@ def test_import():
 
     assert True
 
+
+def test_genanki():
+    """Test that the installed genanki package is my fork. """
+
+    import genanki
+
+    genanki.Deck("26f0a173-57c6-4f41-b1e7-e74ca62832bc", "MyDeck", conf=None)
+
