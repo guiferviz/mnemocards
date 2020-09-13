f41c35b78a8367627f2bd41c412d462c211dbbb5
guiferviz <guiferviz@gmail.com>
2020-09-13 19:35:25 +0200

    Adding needed git volumes.

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,8 @@
 
+# If you want to use github, pull and push commands you need to set the
+# directory in which you want to clone your repos in the next line:
+CARDS_DIR=$HOME/learning
+
 # Ensures that the config file exists.
 # If it does not exists docker is going to mount a directory instead of a file
 # and the program is going to fail.
@@ -10,14 +14,20 @@ fi
 
 # Run the docker image using the following volumes:
 # * ~/.ssh to clone your GitHub repositories. No needed for generating apkgs.
+# * ~/.gitconfig to use your username and email. No needed for generating apkgs.
 # * ~/.gh_key to use the GitHub API. No needed for generating apkgs.
+# * $CARDS_DIR is the path you want to use for cloning the git repositories.
+#   I have all my cards inside the learning dir in my home path. The commands
+#   github, pull and push use this path.
 # * ~/.local/share/Anki2 if you want to import your apkg files. No needed for
 #   generating apkgs.
 # * ~/.mnemocards config file. Necessary.
 # * $PWD current dir with all the text files used to generate cards. Necessary.
 sudo docker run \
     -v ~/.ssh:/root/.ssh:ro \
+    -v ~/.gitconfig:/root/.gitconfig:ro \
     -v ~/.gh_key:/root/.gh_key:ro \
+    -v $CARDS_DIR:$CARDS_DIR \
     -v ~/.local/share/Anki2:/root/.local/share/Anki2 \
     -v ~/.mnemocards:/root/.mnemocards \
     -v $PWD:/workspace \
