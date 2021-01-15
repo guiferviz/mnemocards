6ab2f9a3a51ffe5a5bf43cc2582ff412256ae304
otterman <otterman@otterman.com>
2021-01-15 23:05:06 +0700

    added some unit testing

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -1,6 +1,7 @@
-import mnemocards
 import unittest
-from mnemocards.auto_generate_tsv import get_translation
+import os
+
+from mnemocards import auto_generate_tsv as tsv
 
 
 class CustomModulesTest(unittest.TestCase):
@@ -21,17 +22,52 @@ class CustomModulesTest(unittest.TestCase):
         )
 
 
-class TranslationModuleTestsuit(unittest.TestCase):
-    def setUp(self):
-        self.single_word = "concert"
-        self.multiple_word = ["stiff", "countenance", "yield"]
-        self.translator = get_translation
+class AutoGenerateTsvUnitTestSuit(unittest.TestCase):
 
-    def test_single_word_returns_translation(self):
+    data_dir, word_file = '/home/otterman/python_projects/active/mnemocards/tests/test_card_folder', 'words.txt'
+    word_file_path = [data_dir, word_file]
+    word_list = ['concert', 'shit happens', 'brook no nonsense', 'ridge']
+    word_list_tranlated = ['концерт', 'дерьмо случается',
+                           'не терпеть глупосте', 'гребень']
 
-        translation = self.translator("concert", "en", "ru")
-        for item in translation:
-            self.assertIn("концерт", item.extra_data["translation"][0][0])
+    def test_words_file_scraped(self):
+
+        scraped_words = tsv.scrape_words_from_file(
+            *self.word_file_path)
+        self.assertEqual(scraped_words, self.word_list)
+
+    def test_tsv_are_saved(self):
+
+        test_tsv_config = open("test_tsv_config.tsv", 'r')
+        output_dir = "test_card_folder"
+        language_pair = "en_ru"
+
+        tsv.save_tsv_files([test_tsv_config.read()],
+                           output_dir, language_pair)
+
+        test_tsv_config.seek(0)
+        result_tsv = open("test_card_folder/en_ru.tsv", 'r')
+
+        self.assertEqual(test_tsv_config.read(), result_tsv.read())
+
+        test_tsv_config.close()
+        result_tsv.close()
+        os.remove('test_card_folder/en_ru.tsv')
+
+    def test_tsv_config_generated(self):
+        pass
+
+    def test_words_are_collected(self):
+        args = {'command': 'maketsv', 'data_dir': '.', 'language_pair': 'en_ru',
+                'output_dir': '.', 'recursive': True, 'word_file': 'words.txt'}
+
+        pass
+
+    def test_words_are_translated(self):
+        translations = tsv.get_translation(self.word_list, 'en', 'ru')
+        expected_type = "<class 'googletrans.models.Translated'>"
+        self.assertEqual(str(type(translations[0])), expected_type)
+        self.assertIn(translations[0].text, self.word_list_tranlated)
 
 
 if __name__ == "__main__":
