42be994295c076639a203530f329fc56cb71d0be
otterman <otterman@otterman.com>
2021-01-26 22:27:04 +0700

    Revert "Revert "draft of unit test file""
    This reverts commit 648927ffc98d5fc59cc36f09cc107c2b07915f34.

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -1,6 +1,5 @@
 import unittest
 import os
-import pickle
 
 from mnemocards import auto_generate_tsv as tsv
 
@@ -29,12 +28,7 @@ class AutoGenerateTsvUnitTestSuit(unittest.TestCase):
     word_file_path = [data_dir, word_file]
     word_list = ['concert', 'shit happens', 'brook no nonsense', 'ridge']
     word_list_tranlated = ['концерт', 'дерьмо случается',
-                           'не терпеть глупостей', 'гребень']
-
-    example_translation = {'translation': [['насмехаться', 'scoff', None, None, 3, None, None, [[]], [[['cce7c67b3f2439089dd6b428e0b83b88', 'en_ru_2020q2.md']]]], [None, None, "nasmekhat'sya", 'skôf']], 'all-translations': [['глагол', ['издеваться', 'насмехаться', 'глумиться', 'зубоскалить', 'жрать', 'засмеять', 'есть с жадностью'], [['издеваться', ['scoff', 'mock', 'flout', 'ride', 'guy', 'roast'], None, 0.24126123], ['насмехаться', ['mock', 'taunt', 'scoff', 'razz', 'sneer', 'deride'], None, 0.15822297], ['глумиться', ['mock', 'scoff', 'sneer', 'jeer', 'flout'], None, 0.017752126], ['зубоскалить', ['scoff'], None, 0.00995804], ['жрать', ['fress', 'scoff', 'knock back'], None, 0.0012080474], ['засмеять', ['laugh down', 'scoff'], None, 0.0009706933], ['есть с жадностью', ['gorge', 'raven', 'tuck into', 'guzzle', 'devour', 'scoff'], None, 0.00048052802]], 'scoff', 2], ['имя существительное', ['насмешка', 'жратва', 'посмешище', 'еда'], [['насмешка', ['mockery', 'ridicule', 'sneer', 'taunt', 'jibe', 'scoff'], None, 0.0028979548, None, 2], ['жратва', ['scoff'], None, 0.0019304542, None, 2], ['посмешище', ['mockery', 'laughing-stock', 'joke', 'derision', 'mock', 'scoff'], None, 0.0009706933, None, 3], ['еда', ['food', 'meal', 'eating', 'fare', 'meat', 'scoff'], None, 0.000721358, None, 2]], 'scoff', 1]], 'original-language': 'en', 'possible-translations': [['scoff', None, [['насмехаться', 0, True, False], ['Scoff', 0, True, False]], [[0, 5]], 'scoff', 0, 0]], 'confidence': 1.0, 'possible-mistakes': None, 'language': [['en'], None, [1.0], ['en']], 'synonyms': [['глагол', [[['make sport of'], 'm_en_gbus0909000.007'], [['fleer at', "bite one's thumb at", 'scout at'], 'm_en_gbus0909000.007'], [
-        ['mock', 'deride', 'ridicule', 'sneer at', 'be scornful about', 'treat contemptuously', 'jeer at', 'jibe at', 'make fun of', 'poke fun at', 'laugh at', 'scorn', 'laugh to scorn', 'dismiss', 'pooh-pooh', 'make light of', 'belittle', 'taunt', 'tease', 'make a fool of', 'rag'], 'm_en_gbus0909000.007'], [["thumb one's nose at", 'take the mickey out of', 'poke mullock at'], 'm_en_gbus0909000.007'], [['eat', 'devour', 'consume', 'guzzle', 'gobble', 'wolf down', 'polish off', 'finish off', 'gulp down', 'bolt', 'put away', 'nosh', 'get outside of', 'pack away', 'demolish', 'shovel down', 'stuff(down)', "stuff one's face with", 'stuff oneself with', 'pig oneself on', 'pig out on', 'sink', 'gollop', 'shift', "get one's laughing gear round", 'gorb', 'scarf(down/up)', 'snarf(down/up)', 'inhale'], 'm_en_gbus0909010.007'], [['ingurgitate'], 'm_en_gbus0909010.007']], 'scoff'], ['имя существительное', [[['food', 'fare', 'eatables', 'refreshments', 'grub', 'nosh', 'chow', 'eats', 'feed', 'tuck', 'chuck'], 'm_en_gbus0909010.014'], [['victuals', 'vittles', 'meat'], 'm_en_gbus0909010.014']], 'scoff']], 'definitions': [['глагол', [['speak to someone or about something in a scornfully derisive or mocking way.', 'm_en_gbus0909000.007', '“You, a scientist?” he scoffed'], ['eat(something) quickly and greedily.', 'm_en_gbus0909010.007', 'she scoffed down several chops']], 'scoff'], ['имя существительное', [['an expression of scornful derision.', 'm_en_gbus0909000.014', 'scoffs of disbelief'], ['food.', 'm_en_gbus0909010.014']], 'scoff']], 'examples': [[['his army was the < b > scoff < /b > of all Europe', None, None, None, 3, 'm_en_gbus0909000.017']]], 'see-also': None}
-
-    example_tsv_line = ""
+                           'не терпеть глупосте', 'гребень']
 
     def test_words_file_scraped(self):
 
@@ -74,14 +68,6 @@ class AutoGenerateTsvUnitTestSuit(unittest.TestCase):
         expected_type = "<class 'googletrans.models.Translated'>"
         self.assertEqual(str(type(translations[0])), expected_type)
         self.assertIn(translations[0].text, self.word_list_tranlated)
-
-    def test_translation_converts_to_tsv_line(self):
-        translation = tsv.get_translation('scoff', 'en', 'ru')
-        expected_line = self.example_tsv_line
-        created_line = tsv.create_tsv_line(
-            translation[0])
-        self.assertEqual(created_line, expected_line)
-    # ID	YourLanguageWord	YourLanguageExplanation	LanguageYouLearnWord	LanguageYouLearnPronunciation	LanguageYouLearnExplanation	Tags
 
 
 if __name__ == "__main__":
