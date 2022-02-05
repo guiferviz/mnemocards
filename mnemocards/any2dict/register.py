d2de8af1372b55814b0d81fe825c47221bfeae34
guiferviz <guiferviz@gmail.com>
2022-02-06 00:46:51 +0100

    testing any2dict.register module

--- mnemocards/any2dict/register.py
+++ mnemocards/any2dict/register.py
@@ -1,16 +1,21 @@
+from typing import List, Type
+
 from .reader import Reader
 
-_READERS = []
+_READERS: List[Type] = []
 
 
-def add_reader(reader):
+def add_reader(reader: Type):
     if not issubclass(reader, Reader):
-        raise TypeError(f"{type(reader)} should be a subclass or mnemocards.Reader")
+        raise TypeError(
+            f"{reader.__name__} should be a subclass of "
+            f"{Reader.__module__}.{Reader.__name__}"
+        )
     _READERS.append(reader)
     return reader
 
 
-def get_reader_by_extension(extension):
+def get_reader_by_extension(extension) -> Type:
     for i in _READERS:
         if extension in i.extensions:
             return i
